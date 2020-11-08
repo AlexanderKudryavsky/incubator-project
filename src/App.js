fffc46d8d900694652f1e6b6e999ee559505ac50
GlebPilipenko <gleb.fotodom.by@gmail.com>
2020-11-09 01:53:09 +0300

    commit added search func & repair func which add new point

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react'
-import './App.css';
 import {YandexMap} from './components/yandexMap/yandexMap'
 
-export const App = () =>  {
-  return (
-    <div className="App">
-      <YandexMap/>
-    </div>
-  );
+export const App = () => {
+    return <YandexMap/>
 }
